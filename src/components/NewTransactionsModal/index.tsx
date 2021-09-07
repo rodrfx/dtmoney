df6bb959988f9cfbe5366cd53e573107f7ea282e
Rodrigo Xavier <rodrfx@gmail.com>
2021-09-07 03:13:01 -0300

    component modal-estilizado

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,17 +1,67 @@
+import { useState } from "react";
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
+import { Container, TransactionsTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
-    isOpen: boolean;
-    onRequestClose: () => void;
+  isOpen: boolean;
+  onRequestClose: () => void;
 }
 
-export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    return (
-        <Modal 
-        isOpen={isOpen}
-        onRequestClose= {onRequestClose}
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
       >
-          <h2>Cadastrar Transação</h2>
-        </Modal>
-        )
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+
+        <TransactionsTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionsTypeContainer>
+
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
 }
